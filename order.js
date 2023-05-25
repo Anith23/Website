6ffdffe9441602c7083bf159ba2944d99386a789
Anitha Anitha <88196992+Anith23@users.noreply.github.com>
2023-05-25 16:02:58 +0530

    Delete order.js

--- order.js
+++
@@ -1,8 +0,0 @@
-function calculation(){
-    const coffee = document.getElementById("nocoffee").value;
-    const Price = document.getElementById("coffeeprice").value;
-    const Total = coffee*Price;
-    document.getElementById("total").value = Total;
-
-}
- 
